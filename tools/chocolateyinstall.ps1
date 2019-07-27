96beb5ac4c83aa9953b77feb285aa36042e4ad9c
Lyze <11274700+lyze237@users.noreply.github.com>
2019-07-27 16:01:03 +0200

    updated to 1.8.6

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $name          = 'gopass'
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64         = 'https://github.com/gopasspw/gopass/releases/download/v1.8.5/gopass-1.8.5-windows-amd64.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
-$checksum64    = 'AD662686BC0FB3F514F73E0A79E343EE'
+$url64         = 'https://github.com/gopasspw/gopass/releases/download/v1.8.6/gopass-1.8.6-windows-amd64.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+$checksum64    = '87932040A6D6A9B773358E4E3DC991C1'
 
 Install-ChocolateyZipPackage -PackageName $name -UnzipLocation $toolsDir -Url64bit $url64 -Checksum64 $checksum64
