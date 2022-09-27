077d65cebbe233aba1d67b75cab0ad5d042de1d3
Lyze <11274700+lyze237@users.noreply.github.com>
2022-09-27 21:50:06 +0200

    Updated to 1.14.8

--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 choco pack
-choco install packageName -dv -s .
+choco uninstall gopass
+choco install gopass -dv -s .
 choco push packageName.nupkg -s https://push.chocolatey.org/
+
+https://emn178.github.io/online-tools/md5_checksum.html
--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $name          = 'gopass'
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64         = 'https://github.com/gopasspw/gopass/releases/download/v1.14.5/gopass-1.14.5-windows-amd64.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
-$checksum64    = '274c595be46f43d9c3a828265eb37d87'
+$url64         = 'https://github.com/gopasspw/gopass/releases/download/v1.14.8/gopass-1.14.8-windows-amd64.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+$checksum64    = '4a53b2036ea55befa051bb00839b9fa5'
 
 Install-ChocolateyZipPackage -PackageName $name -UnzipLocation $toolsDir -Url64bit $url64 -Checksum64 $checksum64
