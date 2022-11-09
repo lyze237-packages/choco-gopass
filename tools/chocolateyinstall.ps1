ee011909f02a8eb58fdc0cc9db28ad658cb8e12d
Daniel Jacobs <mail@danieljacobs.de>
2022-11-09 19:14:43 +0100

    Updated to 1.14.10 fixes #2
    Signed-off-by: Daniel Jacobs <mail@danieljacobs.de>

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $name          = 'gopass'
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64         = 'https://github.com/gopasspw/gopass/releases/download/v1.14.8/gopass-1.14.8-windows-amd64.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
-$checksum64    = '4a53b2036ea55befa051bb00839b9fa5'
+$url64         = 'https://github.com/gopasspw/gopass/releases/download/v1.14.10/gopass-1.14.10-windows-amd64.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+$checksum64    = 'af21923cd49dc2d1bf933949c0ad88ff'
 
 Install-ChocolateyZipPackage -PackageName $name -UnzipLocation $toolsDir -Url64bit $url64 -Checksum64 $checksum64
